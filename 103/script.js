b40cebccc925b485a8bb57d68bb5529e8a7e57c1
dannaph26950 <105207377+dannaph26950@users.noreply.github.com>
2023-01-07 08:56:26 +0700

    Create script.js
    103

---
+++ 103/script.js
@@ -0,0 +1,8 @@
+'use strict':
+const restaurant ={
+    name:'Classico Italiano',
+    location :'Via Angelo Tavani 23,Firenze,Italy',
+    categories:['Italian','Pizzeria','Vegetarian','Organic'],
+    starterMenu:['Focaccia','Brushchetta','Garlic Bread','Caprese Salad'],
+    mainMenu:['Pizza','Pasta','Risotto'],
+};
